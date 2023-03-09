679c33531913ea888c48cfa586c4274ed85a6618
Alex <84932536+Derbyshire227@users.noreply.github.com>
2023-03-09 10:28:34 +0000

    Update Menu try 1.php

--- Menu try 1.php
+++ Menu try 1.php
@@ -33,6 +33,11 @@ switch ($choice) {
     echo "Please select a option from the menu";
 
 
+function new_movie($movie_name,$year,$price,$run_time){
+    $movie_name = (string)readline("What is the name of the movie?"); 
+    $year = (int)readline("Please enter the year the movie was released?"); 
+    $run_time = (int)readline("Please enter the run time for the movie (in minutes/rounded up");
+}
 
 
 
